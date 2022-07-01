92f1e112fc0a24eeec76087b17556e438f8d24fc
Fasil Getie <51247312+fasil712@users.noreply.github.com>
2022-07-01 22:01:58 +0300

    update commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart';
 import 'package:badges/badges.dart';
 import 'package:myapp/bmi.dart';
 import 'package:myapp/contact.dart';
