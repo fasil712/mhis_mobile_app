424b03692386a095d65e81268939f30633d72c67
Fasil Getie <51247312+fasil712@users.noreply.github.com>
2022-07-27 16:55:37 +0300

    update commit

---
+++ lib/widgets/item_carousel.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class ItemCarousel extends StatefulWidget {
+  const ItemCarousel({Key? key}) : super(key: key);
+
+  @override
+  State<ItemCarousel> createState() => _ItemCarouselState();
+}
+
+class _ItemCarouselState extends State<ItemCarousel> {
+  int currentIndex = 0;
+  List<String> images = [
+    "assets/food_list/month01.jpg",
+    "assets/food_list/month02.jpg",
+    "assets/food_list/month03.jpg",
+    "assets/food_list/month04.jpg",
+    "assets/food_list/month05.jpg",
+    "assets/food_list/month06.jpg",
+    "assets/food_list/month07.jpg",
+    "assets/food_list/month08.jpg",
+    "assets/food_list/month09.jpg"
+  ];
+  List<String> names = [
+    "Month 01",
+    "Month 02",
+    'Month 03',
+    'Month 04',
+    'Month 05',
+    'Month 06',
+    "Month 07",
+    "Month 08",
+    "Month 09"
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          color: Colors.black12,
+          borderRadius: BorderRadius.all(Radius.circular(20.0))),
+      child: Stack(
+        fit: StackFit.loose,
+        children: [
+          CarouselSlider.builder(
+              itemCount: images.length,
+              itemBuilder: (context, index, next) {
+                return ClipRRect(
+                  child: Container(
+                    height: MediaQuery.of(context).size.height / 3,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                      image: AssetImage(
+                        images[index],
+                      ),
+                      fit: BoxFit.fill,
+                      alignment: Alignment.topCenter,
+                    )),
+                  ),
+                  borderRadius: const BorderRadius.vertical(
+                      top: Radius.circular(32), bottom: Radius.circular(32)),
+                );
+              },
+              //items: ,
+              options: CarouselOptions(
+                  autoPlay: true,
+                  height: 250,
+                  aspectRatio: 18 / 10,
+                  viewportFraction: 1,
+                  initialPage: 0,
+                  reverse: false,
+                  enlargeCenterPage: true,
+                  enableInfiniteScroll: true,
+                  pageSnapping: true,
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enlargeStrategy: CenterPageEnlargeStrategy.height,
+                  autoPlayInterval: const Duration(seconds: 5),
+                  autoPlayAnimationDuration: const Duration(milliseconds: 2000),
+                  pauseAutoPlayOnTouch: true,
+                  scrollDirection: Axis.horizontal,
+                  onPageChanged: (index, next) {
+                    setState(() {
+                      currentIndex = index;
+                    });
+                  })),
+          AspectRatio(
+            aspectRatio: 18 / 10,
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 180.0),
+                child: Text(
+                  names[currentIndex],
+                  style: const TextStyle(
+                      decoration: TextDecoration.none,
+                      color: Color.fromARGB(255, 235, 8, 8),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14.0),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
