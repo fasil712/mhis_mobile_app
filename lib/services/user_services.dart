0e57abc1dbf66d970b88c491269b8a1d3e19daf9
Fasil Getie <51247312+fasil712@users.noreply.github.com>
2022-08-07 20:59:39 +0300

    update commit

--- lib/services/user_services.dart
+++ lib/services/user_services.dart
@@ -1,11 +1,12 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:myapp/api/constants.dart';
 
 import 'package:myapp/model/user_model.dart';
 
 Future<UserModel> registerUser(String facilityname, String mrn, String registrationdate, String firstname, String lastname,  String grandfathername, String age, String sex, String email, String phonenumber, String region,String woreda, String kebele) async {
   final response = await http.post(
-    Uri.parse('http://10.161.168.44:4000/users'),
+    Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndpoint),
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
     },
@@ -33,6 +34,6 @@ Future<UserModel> registerUser(String facilityname, String mrn, String registrat
   } else {
     // If the server did not return a 201 CREATED response,
     // then throw an exception.
-    throw Exception('Failed to create album.');
+    throw Exception('Failed to create user.');
   }
 }
