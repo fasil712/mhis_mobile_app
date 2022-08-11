180005846cdca689b7999c061076c5e3c63b0244
Fasil Getie <51247312+fasil712@users.noreply.github.com>
2022-08-11 16:37:01 +0300

    update commit

--- lib/screens/pregnancey_care/pregnancey_care_screen.dart
+++ lib/screens/pregnancey_care/pregnancey_care_screen.dart
@@ -48,7 +48,7 @@ class _PregnanceyCareState extends State<PregnanceyCare> {
                                         builder: (context) =>
                                             DretailPage(item: item))),
                                 child: CircleAvatar(
-                                  backgroundColor: Colors.blue,
+                                  backgroundColor: Colors.yellow,
                                   radius: 53,
                                   child: CircleAvatar(
                                       radius: 50,
