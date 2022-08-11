3c66eda66dfaf188b60828e261b08ee1faf1086c
Fasil Getie <51247312+fasil712@users.noreply.github.com>
2022-08-11 21:34:54 +0300

    update commit

--- lib/others/constants.dart
+++ lib/others/constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static String baseUrl = 'http://10.161.164.237:3000';
+  static String baseUrl = 'http://localhost:3000';
   static String clientListEndpoint = '/clientList';
   static String addClientEndpoint = '/add';
   static String usersLoginEndpoint = '/login';
