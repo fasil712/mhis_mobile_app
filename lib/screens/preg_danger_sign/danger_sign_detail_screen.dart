cf183aac6026129d473a50e3c80ad72974145d57
Fasil Getie <51247312+fasil712@users.noreply.github.com>
2022-08-12 16:32:29 +0300

    update commit

--- lib/screens/preg_danger_sign/danger_sign_detail_screen.dart
+++ lib/screens/preg_danger_sign/danger_sign_detail_screen.dart
@@ -10,30 +10,32 @@ class DangerSignDetail extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(item.name)),
-      body: Container(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-            child: Column(
-          children: [
-            Image.asset(item.image),
-            const SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              item.name,
-              style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-            ),
-            Text(item.description, style: const TextStyle(fontStyle: FontStyle.italic),),
-            const SizedBox(
-              height: 10.0,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text("Back"))
-          ],
-        )),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+              child: Column(
+            children: [
+              Image.asset(item.image),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                item.name,
+                style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              ),
+              Text(item.description, style: const TextStyle(fontStyle: FontStyle.italic),),
+              const SizedBox(
+                height: 10.0,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Back"))
+            ],
+          )),
+        ),
       ),
     );
   }
