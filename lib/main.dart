3948785b47397ab1f03ed7eb54c101c8c4278d04
Fasil Getie <51247312+fasil712@users.noreply.github.com>
2022-08-15 11:10:11 +0300

    update commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pregmomcare/config/colors.dart';
 import 'package:pregmomcare/screens/profile.dart';
 import 'package:pregmomcare/widgets/drawer.dart';
 import 'package:pregmomcare/screens/home_screen.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
