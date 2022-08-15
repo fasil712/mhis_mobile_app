913f2e70ea9c37bc3704b268cfcd3bafb6d0dd2b
Fasil Getie <51247312+fasil712@users.noreply.github.com>
2022-08-16 00:36:21 +0300

    update commit

--- lib/screens/edd_screen.dart
+++ lib/screens/edd_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pregmomcare/config/colors.dart';
 
 class EDDCalculator extends StatefulWidget {
   const EDDCalculator({Key? key}) : super(key: key);
@@ -13,17 +14,19 @@ class _EDDCalculatorState extends State<EDDCalculator> {
   final _lmpController = TextEditingController();
   final _todayController = TextEditingController();
 
-  Future<void> _selectNowDate(BuildContext context) async {
+  Future<void> _selectTodayDate(BuildContext context) async {
     final DateTime? pickedDate = await showDatePicker(
         context: context,
         initialDate: currentDate,
         firstDate: DateTime(2015),
         lastDate: DateTime(2050));
+
     if (pickedDate != null && pickedDate != currentDate)
       // ignore: curly_braces_in_flow_control_structures
       setState(() {
         currentDate = pickedDate;
         _todayController.text = currentDate.toString();
+        DateUtils.dateOnly(currentDate);
       });
   }
 
@@ -41,13 +44,23 @@ class _EDDCalculatorState extends State<EDDCalculator> {
       });
   }
 
-  String edd = "Null";
-  
-  result() {
-    DateTime dueDate = DateTime.parse(_lmpController.text);
-    //DateTime gaDate = DateTime.parse(_lmpController.text);
+  String dueDate = "2019-10-22 00:00:00.000";
+  String gastetionalAge = "3 Weeks 6 Days";
+
+  result() async {
+    DateTime lmpFromField = DateTime.parse(_lmpController.text);
+    DateTime currentDateFromFild = DateTime.parse(_todayController.text);
+    DateTime minus3Month = lmpFromField.subtract(const Duration(days: 120));
+
+    int days = currentDateFromFild.difference(lmpFromField).inDays;
+
+    // ignore: division_optimization
+    int week = (days / 7).toInt();
+    int day = (days % 7).toInt();
+
     setState(() {
-      edd = dueDate.subtract(const Duration(days: 90)).toString();
+      dueDate = minus3Month.add(const Duration(days: 372)).toString();
+      gastetionalAge = "$week Weeks $day Days";
     });
   }
 
@@ -61,67 +74,199 @@ class _EDDCalculatorState extends State<EDDCalculator> {
         backgroundColor: const Color(0XFF282842),
         title: const Text("Estimated Date of Delivery"),
       ),
-      body: Center(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(8.0),
         child: Form(
           key: _formkey,
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              const Text('LMP'),
-              TextFormField(
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Date is required";
-                  } else {
-                    return null;
-                  }
-                },
-                controller: _lmpController,
-                decoration: InputDecoration(
-                  labelText: "LMP",
-                  hintText: "lmp",
-                  suffix: IconButton(
-                    icon: const Icon(Icons.calendar_month),
-                    onPressed: () => _selectLmpDate(context),
-                  ),
-                  border: const OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10.0),
+              Container(
+                margin: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  borderRadius:
+                      BorderRadius.circular(10), // <= No more error here :)
+                  color: Colors.blue,
+                ),
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    const Text(
+                      'First day of last period',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                     ),
-                  ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    SizedBox(
+                      height: 60,
+                      child: TextFormField(
+                        keyboardType: TextInputType.datetime,
+                        style:
+                            const TextStyle(fontSize: 15, color: Colors.black),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Date is required";
+                          } else {
+                            return null;
+                          }
+                        },
+                        controller: _lmpController,
+                        decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          filled: true,
+                          hintText: "LMP",
+                          suffix: IconButton(
+                            icon: const Icon(
+                              Icons.calendar_month,
+                              color: Colors.blue,
+                            ),
+                            onPressed: () => _selectLmpDate(context),
+                          ),
+                          border: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    const Text(
+                      'Today\'s date',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    SizedBox(
+                      height: 60,
+                      child: TextFormField(
+                        style:
+                            const TextStyle(fontSize: 15, color: Colors.black),
+                        keyboardType: TextInputType.datetime,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Date is required";
+                          } else {
+                            return null;
+                          }
+                        },
+                        controller: _todayController,
+                        decoration: InputDecoration(
+                          fillColor: Colors.white,
+                          filled: true,
+                          hintText: "TODAY",
+                          suffix: IconButton(
+                            icon: const Icon(
+                              Icons.calendar_month,
+                              color: Colors.blue,
+                            ),
+                            onPressed: () => _selectTodayDate(context),
+                          ),
+                          border: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              const Text('TODAY'),
-              TextFormField(
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Date is required";
-                  } else {
-                    return null;
-                  }
-                },
-                controller: _todayController,
-                decoration: InputDecoration(
-                  labelText: "TODAY",
-                  hintText: "today",
-                  suffix: IconButton(
-                    icon: const Icon(Icons.calendar_month),
-                    onPressed: () => _selectNowDate(context),
-                  ),
-                  border: const OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10.0),
+              Container(
+                padding: const EdgeInsets.only(
+                    left: 15, right: 10, top: 10, bottom: 20),
+                child: Row(
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: AppColors.btnColor,
+                          minimumSize: const Size(140, 50)),
+                      onPressed: () {
+                        if (_formkey.currentState!.validate()) {
+                          result();
+                        }
+                      },
+                      child: const Text(
+                        'Calculate',
+                        style: TextStyle(
+                            letterSpacing: 2,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17),
+                      ),
                     ),
-                  ),
+                    const SizedBox(
+                      width: 15.0,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: AppColors.btnColor,
+                          minimumSize: const Size(140, 50)),
+                      onPressed: () {
+                        _lmpController.clear();
+                        _todayController.clear();
+                      },
+                      child: const Text(
+                        'Re-Calculate',
+                        style: TextStyle(
+                            letterSpacing: 2,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  result();
-                },
-                child: const Text('Calculate'),
-              ),
-              Text("Result : $edd")
+              Container(
+                margin: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  borderRadius:
+                      BorderRadius.circular(10), // <= No more error here :)
+                  color: Colors.blue,
+                ),
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    const Text(
+                      "Estimated due date :",
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 5.0,),
+                    Text(
+                      dueDate,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                          color: Colors.yellow,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 5.0,),
+                    const Text(
+                      "Estimated gestational age :",
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 5.0,),
+                    Text(
+                      gastetionalAge,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                          color: Colors.yellow,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ),
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pregmomcare/config/colors.dart';
 import 'package:pregmomcare/screens/bmi_screen.dart';
 import 'package:pregmomcare/screens/diet_during_preg_screen.dart';
+import 'package:pregmomcare/screens/edd_screen.dart';
 import 'package:pregmomcare/screens/pregnancey_care_screen.dart';
 import 'package:pregmomcare/screens/pregnancy_danger_sign_screen.dart';
 import 'package:pregmomcare/widgets/doctor_list.dart';
@@ -249,6 +250,59 @@ class _HomePageState extends State<HomePage> {
                                 Padding(
                                   padding: EdgeInsets.only(left: 8, right: 8),
                                   child: Text("Body Mass Index Calulator Desc"),
+                                )
+                              ],
+                            ),
+                          ))
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                      builder: (BuildContext context) =>
+                          const EDDCalculator())),
+                  child: Card(
+                    elevation: 5,
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                    child: Container(
+                      padding: const EdgeInsets.all(8),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const CircleAvatar(
+                            backgroundColor: AppColors.appbarBgColor,
+                            radius: 30,
+                            child: CircleAvatar(
+                              radius: 28,
+                              backgroundImage: AssetImage(
+                                "assets/bmi.jpg",
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                              child: Container(
+                            padding: const EdgeInsets.only(bottom: 8),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Padding(
+                                  padding: EdgeInsets.only(left: 8, right: 8),
+                                  child: Text(
+                                    "Estimated Date of Delivery Calulator",
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(left: 8, right: 8),
+                                  child: Text(
+                                      "Estimated Date of Delivery Calulator Desc"),
                                 )
                               ],
                             ),
